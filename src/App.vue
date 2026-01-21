c7f912c2f55e346df36365dd38f76cb152d07d44
emynhrx <emynbusiness@gmail.com>
2026-01-21 20:00:37 +0100

    part 1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <p>dddddddd</p>
+  <Home />
 </template>
+
+<script setup>
+import Home from './page/Home.vue'
+</script>
+
+<style>
+body, html, #app {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  scroll-behavior: smooth; 
+  background-color: #f2e9d0;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../page/Home.vue'
+
+const routes = [
+  { path: '/', component: Home }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 } 
+  }
+})
+
+export default router
